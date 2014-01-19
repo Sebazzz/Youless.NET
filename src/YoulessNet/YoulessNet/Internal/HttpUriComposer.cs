925cb3f1ffaf2101f7fcb16a5b0914402f5da11c
Sebazzz <triadsebas@gmail.com>
2014-01-19 22:29:53 +0100

    Fixed fxcop warnings in HttpUriComposer.cs

--- src/YoulessNet/YoulessNet/Internal/HttpUriComposer.cs
+++ src/YoulessNet/YoulessNet/Internal/HttpUriComposer.cs
@@ -1,33 +1,13 @@
 ﻿namespace YoulessNet.Internal {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Text;
 
     /// <summary>
     /// Helper class for composing <see cref="Uri"/> instances (HTTP only)
     /// </summary>
     internal static class HttpUriComposer {
-        /// <summary>
-        /// Composes an <see cref="Uri"/> from the specified parameters
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public static Uri Compose(string path) {
-            return Compose(path, (string) null);
-        }
-
-        /// <summary>
-        /// Composes an <see cref="Uri"/> from the specified parameters
-        /// </summary>
-        /// <param name="path"></param>
-        /// <param name="query">Anonymous object descibing the properties</param>
-        /// <returns></returns>
-        public static Uri Compose(string path, object query) {
-            IDictionary<string, object> queryKeyValues = query != null ? ObjectUtils.GetProperties(query) : null;
-
-            return Compose(path, queryKeyValues);
-        }
-
         /// <summary>
         /// Composes an <see cref="Uri"/> from the specified parameters
         /// </summary>
@@ -63,7 +43,7 @@
 
                     string encodedValue = Uri.EscapeDataString(value);
 
-                    queryBuilder.AppendFormat("{0}={1}&", key, encodedValue);
+                    queryBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0}={1}&", key, encodedValue);
                 }
 
                 if (queryKeyValues.Count > 0) {
